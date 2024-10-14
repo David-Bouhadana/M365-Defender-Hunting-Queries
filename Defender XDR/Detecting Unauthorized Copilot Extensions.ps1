b47723edc71cffa1eb7d98fbb4066e22f020623e
M365 Journey <105270820+David-Bouhadana@users.noreply.github.com>
2024-10-14 09:32:43 +0200

    Create Detecting Unauthorized Copilot Extensions.ps1

---
+++ Defender XDR/Detecting Unauthorized Copilot Extensions.ps1
@@ -0,0 +1,11 @@
+// Detecting new Copilot extensions
+// Détection des nouvelles extensions Copilot
+let WLExtensions = dynamic(["BingWebSearch"]);
+CloudAppEvents
+| where Timestamp > ago(1h)
+| where ActionType == "CopilotInteraction"
+| extend UserID = tostring(RawEventData.UserId),
+         CopilotData = todynamic(RawEventData.CopilotEventData)
+| extend CopilotPlugin = tostring(CopilotData.AISystemPlugin.Id)
+| where isnotempty(CopilotPlugin) and not(CopilotPlugin in (WLExtensions))
+| project Timestamp, AccountObjectId, UserID, CopilotPlugin, ReportId
